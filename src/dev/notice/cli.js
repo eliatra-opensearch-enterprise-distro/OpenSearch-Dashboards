2aa0d47965754dcf721c32ddad350c18194bede5
Kajetan Nobel <kajetan.nobel@eliatra.com>
2024-02-28 22:45:39 +0100

    fix: notice generation
    Signed-off-by: Kajetan Nobel <kajetan.nobel@eliatra.com>

--- src/dev/notice/cli.js
+++ src/dev/notice/cli.js
@@ -80,7 +80,7 @@ if (opts.help) {
 (async function run() {
   const path = resolve(REPO_ROOT, 'NOTICE.txt');
   const newContent = await generateNoticeFromSource({
-    productName: 'OpenSearch (https://opensearch.org/)',
+    productName: 'Copyright Eliatra',
     directory: REPO_ROOT,
     log,
   });
