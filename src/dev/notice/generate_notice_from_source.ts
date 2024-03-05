b80a9f0c8c0b098981acaefed9692a53b6c4c238
Kajetan Nobel <kajetan.nobel@eliatra.com>
2024-03-05 22:02:13 +0100

    Fix workflows (#13)
    * feat: remove changelog verifier
    
    Signed-off-by: Kajetan Nobel <kajetan.nobel@eliatra.com>
    
    * fix: notice generation
    
    Signed-off-by: Kajetan Nobel <kajetan.nobel@eliatra.com>
    
    ---------
    
    Signed-off-by: Kajetan Nobel <kajetan.nobel@eliatra.com>

--- src/dev/notice/generate_notice_from_source.ts
+++ src/dev/notice/generate_notice_from_source.ts
@@ -33,9 +33,7 @@ import { ToolingLog } from '@osd/dev-utils';
 
 const NOTICE_COMMENT_RE = /\/\*[\s\n\*]*@notice([\w\W]+?)\*\//g;
 const NEWLINE_RE = /\r?\n/g;
-const NOTICE_TEXT = `Copyright Eliatra
-
-This product includes software developed by Amazon.com, Inc.
+const NOTICE_TEXT = `This product includes software developed by Amazon.com, Inc.
 (https://opensearch.org/)
 
 This product includes software developed by
